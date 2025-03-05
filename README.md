8a2934cb2f1f64dcad498addf7f0ae9db1b0cb78
Rob Chambers <robchamberspfc@gmail.com>
2025-03-05 09:34:41 +0000

    add new pages script to make things easier to create

--- README.md
+++ README.md
@@ -17,8 +17,19 @@ On update to main, the site will regenerate the HTML from markdown and deploy th
 
 ### Markdown converter
 
-Reads the content of the markdown files in the `helpers//input` directory and converts them to HTML. Output HTML files are pushed to `../_includes/` for use in the app
+Reads the content of the markdown files in the `helpers/input` directory and converts them to HTML. Output HTML files are pushed to `../_includes/` for use in the app
 
 Run manually using:
 
 `node helpers/convert-markdown.js`
+
+### New pages
+
+Automates the creation of new pages based on the content of `new-pages.json`.
+
+**NOTE: Due to the way Eleventy build templates you need to make sure the markdown file has been added to `helpers/input`and Markdown converter has been run before running.**
+
+Run manually using:
+`node helpers/new-pages.js`
+
+Currently uses one page template, could be extended to work with multiple templates, defined within the json file if required. Safe to be rerun with existing pages.
---
+++ helpers/new-pages.js
@@ -0,0 +1,77 @@
+const fs = require('node:fs');
+
+//load config js file
+fs.readFile(`./helpers/new-pages.json`, 'utf8', (err, newPages) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    newPages = JSON.parse(newPages)
+
+    //loop through
+    for (i = 0; i < newPages.length; i++) {
+        //check markdown page exists
+        fs.readFile(`./_includes/markdown-${newPages[i].fileName}.html`, 'utf8', (err, newPages) => {
+            if (err) {
+            console.log(`${err}
+                Markdown file missing. Ensure it exists and convert-markdown.js has been run.`)}
+        });
+
+        //create folder structure
+        fs.mkdirSync(newPages[i].location, {
+            recursive: true
+        }, (err) => {
+            if (err) throw err;
+        });
+
+        //create location page
+        let locationPage = `---
+layout: ${newPages[i].fileName}
+---
+`
+        //write page to location
+        fs.writeFileSync(`${newPages[i].location}/index.html`, locationPage);
+
+        // create basic include page
+        //create breadcrumb
+        breadcrumb = ''
+
+        for (j = 0; j < newPages[i].breadcrumb.length; j++) {
+            breadcrumb += `
+                <li class="breadcrumb-item"><a href="${newPages[i].breadcrumb[j].url}">${newPages[i].breadcrumb[j].title}</a></li>`
+        }
+
+        breadcrumb += `
+                <li class="breadcrumb-item active" aria-current="page">${newPages[i].name}</li>`
+
+        let includePage = `---
+layout: basic
+title: ${newPages[i].name}
+---
+
+<main role="main" id="content" tabindex="-1">
+    <div class="container">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>${breadcrumb}
+            </ol>
+        </nav>
+        <h1 class="pb-4">${newPages[i].name}</h1>
+        <div class="row">
+            <div class="col-md-7">
+                <h1 class="pb-4"></h1>
+                    <div class="row pb-2">
+                        {% include "markdown-${newPages[i].fileName}.html" %}
+                    </div>
+            </div>
+            <div class="col-md-5 ">
+            </div>
+        </div>
+    </div>
+</main>`
+        
+        //write page to location
+        fs.writeFileSync(`./_includes/${newPages[i].fileName}.html`, includePage);
+    }
+})
