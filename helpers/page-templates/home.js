57d891c1b66d006839bc99023f526b15900be1d2
Rob Chambers <robchamberspfc@gmail.com>
2025-03-24 16:19:18 +0000

    add config for top section of homepage

--- helpers/page-templates/home.js
+++ helpers/page-templates/home.js
@@ -1,5 +1,28 @@
 module.exports = (data) => {
 
+//create cards
+topSection = ''
+
+for (k = 0; k < data.topSection.items.length; k++) {
+    topSection += `
+            <div class="col-12 col-md-4 p-md-4">
+                <div class="row pb-2">
+                    <div class="card mb-4 card-navigation rounded-0">
+                        <div class="card-body">
+                            <div class="col-md-10">
+                                <h3 class="card-text">
+                                <a href="${data.topSection.items[k].url}" class="stretched-link card-navigation">
+                                    ${data.topSection.items[k].title}</a></h3>
+                                <p class="card-text">${data.topSection.items[k].description}</p>
+                            </div>
+                            <p class="pb-0 pt-5"><i class="bi bi-arrow-right-short h2"></i></p>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
+
+
     html = `---
 layout: basic
 title: Home
@@ -14,52 +37,8 @@ title: Home
                     social housing sector.</p>
             </div>
         </div>
-        <h2 class="mt-5 mb-5">Get started</h2>
-        <div class="row mb-3">
-            <div class="col-12 col-md-4 p-md-4">
-                <div class="row pb-2">
-                    <div class="card mb-4 card-navigation rounded-0">
-                        <div class="card-body">
-                            <div class="col-md-10">
-                                <h3 class="card-text h3">
-                                <a href="what-are-data-standards" class="stretched-link card-navigation">
-                                    What are data standards?</a></h3>
-                            </div>
-                            <p class="pb-0 pt-5"><i class="bi bi-arrow-right-short h2"></i></p>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-4 p-md-4">
-                <div class="row pb-2 ">
-                    <div class="card mb-4 card-navigation rounded-0">
-                        <div class="card-body">
-                            <div class="col-md-10">
-                                <h3 class="card-text">
-                                <a href="about" class="stretched-link card-navigation">
-                                    About this service</a></h3><br>
-                            </div>
-                            <p class="pb-0 pt-5"><i class="bi bi-arrow-right-short h2"></i></p>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-4 p-md-4">
-                <div class="row pb-2">
-                    <div class="card mb-4 card-navigation rounded-0">
-                        <div class="card-body">
-                            <div class="col-md-10">
-                                <h3 class="card-text">
-                                <a href="procurement" class="stretched-link card-navigation">
-                                    Case studies</a></h3><br>
-                            </div>
-                            <p class="pb-0 pt-5"><i class="bi bi-arrow-right-short h2"></i></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <h2 class="mt-5 mb-5">${data.topSection.name}</h2>
+        <div class="row mb-3">${topSection}
         </div>
         <h2 class="mt-5 mb-5">Get standards</h2>
         <div class="row mb-5 pb-5">
